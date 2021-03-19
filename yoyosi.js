1e6a77b6e914b679478f5e295bffb9b6cc952202
wsndlw <65400784+wsndlw@users.noreply.github.com>
2021-03-19 11:51:31 +0800

    Update yoyosi.js

--- yoyosi.js
+++ yoyosi.js
@@ -1,3 +1,9 @@
-http=a1.ap.fastqvpn.com:29980, username=411450183142866944, password=648d360d-b02a-4c74-b4ac-24adb77ac578, over-tls=true, tls-host=a1.ap.fastqvpn.com, tls-verification=true, fast-open=false, udp-relay=false, tag=一键连1
-http=a2.ap.fastqvpn.com:29980, username=411450183142866944, password=648d360d-b02a-4c74-b4ac-24adb77ac578, over-tls=true, tls-host=a2.ap.fastqvpn.com, tls-verification=true, fast-open=false, udp-relay=false, tag=一键连2
-http=a3.ap.fastqvpn.com:29980, username=411450183142866944, password=648d360d-b02a-4c74-b4ac-24adb77ac578, over-tls=true, tls-host=a3.ap.fastqvpn.com, tls-verification=true, fast-open=false, udp-relay=false, tag=一键连3
+http=a1.ap.fastqvpn.com:29980, username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false,fast-open=false, udp-relay=false, tag=香港
+http=ap1-jp.er3.xyz:29985, username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false,fast-open=false, udp-relay=false, tag=日本
+http=ap1-us.er3.xyz:29988, username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false,fast-open=false, udp-relay=false, tag=美国
+http=ap1-sg.er3.xyz:29986, username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false,fast-open=false, udp-relay=false, tag=新加坡
+http=ap1-uk.er3.xyz:29987, username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false,fast-open=false, udp-relay=false, tag=英国
+http=a1.er3.xyz:29980,username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false, fast-open=false, udp-relay=false, tag=新加坡
+http=ap1-ge.er3.xyz:29984, username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false, fast-open=false, udp-relay=false, tag=德国
+http=ap1-ca.er3.xyz:29983, username= 411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false, fast-open=false, udp-relay=false, tag=加拿大
+http=ap1-au.er3.xyz:29982, username=411450183142866944, password= 648d360d-b02a-4c74-b4ac-24adb77ac57, over-tls=true, tls-verification=false, fast-open=false, udp-relay=false, tag=澳大利亚
